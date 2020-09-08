9059b5fb29f24acdce6b3cc161e9c96a06f562c3
Jitorun <1006104285@qq.com>
2020-09-08 21:09:48 +0800

    完善了处理原文的功能

--- 031802120/first draft.py
+++ 031802120/first draft.py
@@ -1,30 +1,34 @@
 import jieba
-import gensim
+import re
+from gensim import corpora,models,similarities
 def read_in_copy_txt(txtname):
     global copyword
     file=open(txtname,"r",encoding="utf-8")
+    r = '[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\n。！, ]+'
     copyword=file.read()
+    copyword=re.sub(r,'',copyword)
     file.close()
+    print(copyword)
 def read_in_original_txt(txtname):
     global original
     file=open(txtname,"r",encoding="utf-8")
+    r = '[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\n。！， ]+'
     original=file.read()
+    original=re.sub(r,'',original)
+    print(original)
     file.close()
 def dojiebacopy():
     global copytxt
-    copytxt=[]
-    #copy_txt.append(copyword)
-    for doc in copyword:
-        doc_list=[word for word in jieba.cut(doc)]
-        copytxt.append(doc_list)
+    # copy_txt.append(copyword)
+    copytxt = [word for word in jieba.cut(copyword)]
+   # copytxt.append(doc_list)
     print(copytxt)
 def dojiebaoriginal():
     global originaltxt
     originaltxt=[]
     #originaltxt.append(original)
-    for doc in original:
-        doc_list=[word for word in jieba.cut(doc)]
-        originaltxt.append(doc_list)
+    originaltxt=[word for word in jieba.cut(original)]
+    #originaltxt.append(doc_list)
     print(originaltxt)
 read_in_copy_txt("copy.txt")
 read_in_original_txt("original-text.txt")
@@ -32,4 +36,3 @@ dojiebacopy()
 dojiebaoriginal()
 #jieba后抄袭文件为copytxt
 #jieba后原来文件为originaltxt
-
